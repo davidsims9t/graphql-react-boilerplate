de2dcceeb640344d9677bf3b7009df1574b4320a
David Sims <davidesims@icloud.com>
2017-02-18 01:08:34 -0800

    added mongoose and mongodb.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # GraphQL, Apollo Client, and React Boilerplate
 
-Uses GraphQL and Express for the back-end, Apollo client as the client, and
-React for the front-end.
+Uses GraphQL and Express for the back-end, MongoDB for the database, Mongoose as the ODM, Apollo client as the client, and React for the front-end framework.
+
+Create a .env file after you've checked out this repo and use this as a template:
+
+```
+MONGO_URI=mongodb://user:password@myhost/collection
+PORT=4000
+```
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const schema = require('./schema');
 const app = express();
+
+// Global variables
+require('dotenv').load();
+
+// Mongoose connection
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.once('open', () => console.log('Mongoose connection open'))
 
 // GraphQL endpoint with graphiql
 app.use('/graphql', expressGraphQL({
@@ -9,4 +18,4 @@ app.use('/graphql', expressGraphQL({
   graphiql: true
 }));
 
-app.listen(4000, () => console.log('Running on port 4000'));
+app.listen(process.env.PORT, () => console.log('Running on port ' + process.env.PORT));
