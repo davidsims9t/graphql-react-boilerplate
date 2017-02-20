887614c5c07178ed5f1fd34f80b504db33e9cbdc
David Sims <davidesims@icloud.com>
2017-02-20 12:23:32 -0800

    added files for mutations and queries. Implemented flow for static type checking.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GraphQL, Apollo Client, and React Boilerplate
 
-Uses GraphQL and Express for the back-end, MongoDB for the database, Mongoose as the ODM, Apollo client as the client library, and React for the front-end framework, Webpack as the front-end build tool, Bootstrap for the styling, and Jest for unit testing.
+Uses GraphQL and Express for the back-end, MongoDB for the database, Mongoose as the ODM, Apollo client as the client library, and React for the front-end framework, Webpack as the front-end build tool, Bootstrap for the styling, Jest for unit testing, and Flow for static-type checking.
 
 ## Configuration
 
@@ -21,6 +21,8 @@ npm run dev
 
 ## Running Tests
 
-TBC
+Use jest --updateSnapshot to update your Jest snapshots prior to testing. Then use npm test to run jest.
 
-#
+## Testing with Flow
+
+Simply use npm run flow.
---
+++ app/components/AddCompany.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { PageHeader, Form, FormGroup, Button, ControlLabel, FormControl } from 'react-bootstrap';
+import { Link, hashHistory } from 'react-router';
+
+import query from '../queries/users';
+
+class AddCompany extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: ''
+    };
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        fullName: this.state.fullName,
+        age: +this.state.age
+      },
+      refetchQueries: [{ query }]
+    }).then(() => {
+      hashHistory.push('/users');
+    }).catch(() => {
+      // error...
+    });
+  }
+
+  render() {
+    return (
+      <section className="add-user">
+        <PageHeader>
+          Add User
+        </PageHeader>
+
+        <Form onSubmit={this.onSubmit.bind(this)}>
+          <FormGroup controlId="formInlineName">
+            <ControlLabel>Full Name</ControlLabel>
+            <FormControl type="text" onChange={this.onChangeFullName.bind(this)} value={this.state.fullName} />
+          </FormGroup>
+
+          <FormGroup controlId="formInlineName">
+            <ControlLabel>Full Name</ControlLabel>
+            <FormControl type="text" onChange={this.onChangeAge.bind(this)} value={this.state.age} />
+          </FormGroup>
+
+          <Button type="submit">Add User</Button>
+        </Form>
+
+        <Link to="/users" className="user-back-btn btn btn-primary">Go Back</Link>
+      </section>
+    )
+  }
+}
+
+const mutation = gql`
+  mutation AddCompany() {
+    addCompnay() {
+      fullName
+      age
+    }
+  }
+`;
+
+export default graphql(mutation)(AddCompany);
